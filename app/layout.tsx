dcc9637324f1d766035e6422b6aaf796d0029930
jerome-orio <jerome.orio@gmail.com>
2024-09-30 21:53:38 +0800

    Updated metadata.

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,14 +16,31 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Jerome Orio - Software Developer Portfolio",
-  description: "Welcome to my personal portfolio. I am a software developer skilled in JavaScript, React, and Node.js. Explore my projects and get to know my work.",
+  title: "Jerome Orio | Expert Software Developer Portfolio",
+  description: "Welcome to the portfolio of Jerome Orio, a skilled software developer specializing in Java, JavaScript, React, Next.js, Angular, Svelte, TypeScript, Node.js, Oracle, and NoSQL databases. Discover innovative projects and my approach to web development.",
+  keywords: [
+    "Jerome Orio",
+    "Software Developer",
+    "Portfolio",
+    "Java Developer",
+    "JavaScript Developer",
+    "React Developer",
+    "Next.js Expert",
+    "Angular Developer",
+    "Svelte Developer",
+    "Web Development Portfolio",
+    "TypeScript Programmer",
+    "Node.js Developer",
+    "Oracle Developer",
+    "NoSQL Database",
+    "Tailwind CSS",
+    "Software Engineer"
+  ],
   icons: [{ rel: 'icon', url: Favicon.src }],
-  keywords: ["Jerome Orio", "Software Developer", "Portfolio", "JavaScript", "React", "Next.js", "Web Development"],
   authors: [{ name: "Jerome Orio", url: "https://jerome-orio-portfolio.vercel.app/" }],
   openGraph: {
-    title: "Jerome Orio - Software Developer Portfolio",
-    description: "Welcome to my personal portfolio. I am a software developer skilled in JavaScript, React, and Node.js. Explore my projects and get to know my work.",
+    title: "Jerome Orio | Expert Software Developer Portfolio",
+    description: "Welcome to the portfolio of Jerome Orio, a skilled software developer specializing in Java, JavaScript, React, Next.js, Angular, Svelte, TypeScript, Node.js, Oracle, and NoSQL databases. Discover innovative projects and my approach to web development.",
     type: "website",
     url: "https://jerome-orio-portfolio.vercel.app/",
     locale: "en_US",
